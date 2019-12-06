408f9a4f28636041b26ade4576f8fe52a04b5379
Serhii Osadchuk <serhiy.osadchuk@gmail.com>
2019-12-06 14:56:11 +0200

    First App in Sumilator

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,17 @@
 
 import 'package:flutter/material.dart';
 
-
-
 void main(){
-
-
-
-
+runApp(MyFirstApp());
 
 }
 
 
 
 class MyFirstApp extends StatelessWidget {
-
   Widget build(BuildContext context) {
     // TODO: implement build
-    return null;
+    return MaterialApp(home: Text('Hello'),);
   }
 
 }
