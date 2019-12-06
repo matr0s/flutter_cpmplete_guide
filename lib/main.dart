519538b6b67c910729f94783ed9b6172a4a92f94
Serhii Osadchuk <serhiy.osadchuk@gmail.com>
2019-12-06 14:35:21 +0200

    Test commint for VC

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,23 @@
+
+import 'package:flutter/material.dart';
+
+
+
 void main(){
 
 
 
 
+
+}
+
+
+
+class MyFirstApp extends StatelessWidget {
+
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
 
 }
