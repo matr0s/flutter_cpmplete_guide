bc366f56ca1cc54eaf032c88f070f0f428aacd05
Serhii Osadchuk <serhiy.osadchuk@gmail.com>
2019-12-31 14:55:33 +0200

    section 51: not clear

--- lib/main.dart
+++ lib/main.dart
@@ -18,21 +18,36 @@ class _MyAppState extends State<MyApp> {
   final _questions = const [
     {
       'questionText': 'What is your fave color?',
-      'answers': ['Red', 'White', 'Green'],
+      'answers': [
+        {'answerText': 'Red', 'score': 10},
+        {'answerText': 'White', 'score': 5},
+        {'answerText': 'Green', 'score': 3},
+      ],
     },
     {
       'questionText': 'What is your fave car?',
-      'answers': ['BMW', 'AZLK', 'KIA', 'Ford'],
+      'answers': [
+        {'answerText': 'BMW', 'score': 10},
+        {'answerText': 'AZLK', 'score': 5},
+        {'answerText': 'KIA', 'score': 3},
+        {'answerText': 'Ford', 'score': 1},
+      ],
     },
     {
       'questionText': 'What is your fave song?',
-      'answers': ['DRDre', 'XAC', 'Green Gray'],
+      'answers': [
+        {'answerText': 'DRDre', 'score': 10},
+        {'answerText': 'XAC', 'score': 5},
+        {'answerText': 'Green Gray', 'score': 3},
+      ],
     },
   ];
 
   var _questionIndex = 0;
+  var _totalScore = 0;
 
-  void _answerQuestion() {
+  void _answerQuestion(int score) {
+    _totalScore = _totalScore + score;
     setState(() {
       _questionIndex += 1;
     });
